abc50a82c91210ef496e1822a7ab8a50a4464971
Connor <connor.doherty97@gmail.com>
2022-08-23 17:42:03 -0400

    main.js delete function added

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,28 @@
 const delBtn = document.querySelectorAll('.del')
+
+Array.from(delbtn).forEach(element=>{
+    element.addEventListener('click',deleteDream)
+})
+
+
+
+
+
+
+async function deletedream(){
+    const dreamId = this.parentNode.dataset.id 
+    try{
+        const response = await fetch ('journal/deleteDream', {
+            method:'delete',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'dreamIdFromJSFile':dreamId 
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}

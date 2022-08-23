89d350c83f5edb8ea416de354634c3858eae1579
Connor <connor.doherty97@gmail.com>
2022-08-22 21:12:57 -0400

    basic arch for server.js done

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 const express = require ('express');
 const app = express()
 const connectDB = require('./config/database')
+const homeRoutes = require('./routes/home')
+const todaysRoute = require('./routes/today')
+const customRoute = require('./routes/custom')
+
+require('dotenv').config({path: './config/.env'})
+
+connectDB()
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use('/', homeRoutes)
+app.use('/today', todaysRoute)
+app.use('/custom', customRoute)
+ 
+app.listen(process.env.PORT, ()=>{
+    console.log('Server is running, you better catch it!')
+})    

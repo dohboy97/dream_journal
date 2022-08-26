af9f80c46d5da04221d57070df76c07a2abb7c31
Connor <connor.doherty97@gmail.com>
2022-08-26 15:58:06 -0400

    text editor  quill initialized on editing page

--- public/css/tailwind.config.js
+++ public/css/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../index.html",'../../views/index.ejs','../../views/journal.ejs','../../views/journal/edit.ejs'],
+  content: ["../index.html",'../../views/index.ejs','../../views/journal.ejs','../../views/edit.ejs'],
   theme: {
     extend: {},
   },

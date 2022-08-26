471d50e0b8391a71f0793eeac78a7bfe8f1e7b41
Connor <connor.doherty97@gmail.com>
2022-08-26 14:36:23 -0400

    delete functionality now working on the editing page

--- controllers/edit.js
+++ controllers/edit.js
@@ -24,6 +24,7 @@ module.exports = {
             })
             console.log(dreamEntry)
             res.render('edit.ejs', {dreams:dreamEntry})
+            
         }catch(err){
             console.log(err)
         }
--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,32 @@
 
 const delBtn = document.querySelectorAll('.del')
 
+const editBtn = document.querySelectorAll('.edit')
+
 Array.from(delBtn).forEach(element=>{
     element.addEventListener('click',deleteDream)
 })
 
+Array.from(editBtn).forEach(element=>{
+    element.addEventListener('click',editDream)
+})
 
 
 
+// async function editDream(){
+//     const dreamID = this.parentNode.dataset.id
+//     try{
+//         const response = await fetch(`/journal/edit/${dreamID}`,{
+//             method:'put',
+            
+//         })
+//         const data = await response.json()
+//         console.log(data)
+     
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
 
 
 async function deleteDream(){

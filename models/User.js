8accf140cd7b359e2fc8fbf3e74d17d60ac047d0
Connor <connor.doherty97@gmail.com>
2022-09-08 14:29:02 -0400

    basic user mongoose schema done

---
+++ models/User.js
@@ -0,0 +1,9 @@
+const bcrpyt = bcrpyt
+
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema({
+    userName: { type: String, unique: true},
+    email: { type: String, unique: true},
+    password: String
+})

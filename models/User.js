f56c79e3e2546e0fffa43a41e7b2c1b5c0e43c0b
Connor <connor.doherty97@gmail.com>
2022-09-08 14:36:19 -0400

    bcrypt serializing

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const bcrpyt = bcrpyt
+const bcrypt = bcrypt
 
 const mongoose = require('mongoose')
 
@@ -6,4 +6,19 @@ const UserSchema = new mongoose.Schema({
     userName: { type: String, unique: true},
     email: { type: String, unique: true},
     password: String
+})
+
+UserSchema.pre('save', function save(next){
+    const user = this
+
+    if(!user.isModified('password')){ return next()}
+    bcrypt.genSalt(10, (err, salt)=> {
+        if(err) {return next(err)}
+        bcrypt.hash(user.password, salt, (err, hash)=> {
+            if(err) { return next(err)}
+            user.password = hash
+            next()
+        })
+    })
+
 })

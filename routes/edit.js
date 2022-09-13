398694d8c2c864fffa2f8801b3a23d62b3480c41
Connor <connor.doherty97@gmail.com>
2022-09-13 00:41:43 -0400

    edit page ensure auth added

--- routes/edit.js
+++ routes/edit.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { append } = require('express/lib/response')
-
+const authController = require('../controllers/auth')
 const router = express.Router()
 const editController = require('../controllers/edit')
+const ensureAuth  = require('../middleware/auth')
 
-router.get('/:id', editController.getDreamById)
+router.get('/:id', ensureAuth.ensureAuth, editController.getDreamById)
 
 router.delete('/deleteDream', editController.deleteDream)
 

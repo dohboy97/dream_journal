ed602c323c6254f27c7c9e4379e15d5556631712
Connor <connor.doherty97@gmail.com>
2022-09-13 15:51:49 -0400

    tailwind added

--- public/css/tailwind.config.js
+++ public/css/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["../index.html",'../../views/index.ejs','../../views/journal.ejs','../../views/edit.ejs'],
+  content: ["../index.html",'../../views/index.ejs','../../views/journal.ejs','../../views/edit.ejs','../../views/entries.ejs','../../views/login.ejs','../../views/signup.ejs'],
   theme: {
     extend: {},
   },

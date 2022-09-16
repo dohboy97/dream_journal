1ca2aee6292f8290a0239ebe73d40c1de83299d9
Connor <connor.doherty97@gmail.com>
2022-09-16 13:25:52 -0400

    nav done

--- public/css/tailwind.config.js
+++ public/css/tailwind.config.js
@@ -2,8 +2,14 @@
 module.exports = {
   content: ["../../views/index.ejs",'../../views/index.ejs','../../views/journal.ejs','../../views/edit.ejs','../../views/entries.ejs','../../views/login.ejs','../../views/signup.ejs', '../../views/info.ejs', '../../views/viewer.ejs'],
   theme: {
-    main: {
-    padding:'3rem'
+    borderWidth: {
+      DEFAULT: '1px',
+      '1': '1px',
+      '2': '2px',
+      '3': '3px',
+      '4': '4px',
+      '6': '6px',
+      '8': '8px',
     }
   },
   plugins: [],
